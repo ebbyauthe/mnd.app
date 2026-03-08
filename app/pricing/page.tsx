31643a1153a48d2a596995ca7ed500610fb3e547
ebbyauthe <samuelauthe@gmail.com>
2026-03-08 06:19:50 -0600

    app/pricing/page.tsx

---
+++ app/pricing/page.tsx
@@ -0,0 +1,9 @@
+import PricingSection from "@/app/components/pricing-section";
+
+export default function PricingPage() {
+  return (
+    <main className="min-h-[calc(100vh-56px)] bg-[#020817]">
+      <PricingSection />
+    </main>
+  );
+}
